cc8becabeb4dd296405ec493f0f7947463023676
Nielsen-Mudaki <jumbanielsen@gmail.com>
2022-01-19 22:56:24 +0300

    remove label

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from "../user";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  user!: User;
 
   ngOnInit(): void {
+    interface ApiResponse{
+      name:string;
+    }
+    this.http.get<ApiResponse>("https://api.github.com/user").subscribe((data)=>{
+        // Succesful API request
+        this.user = new User(data.name)
+      })
   }
+
+  
 
 }
